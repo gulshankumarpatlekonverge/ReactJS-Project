b2ec44f592d0afbfbfcc91f6dc47e48390ffef80
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-16 16:04:39 +0530

    manual approach to filter category

--- 5-Menu/src/App.jsx
+++ 5-Menu/src/App.jsx
@@ -6,11 +6,29 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  const [item, setItem] = useState(items)
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const fileterItems = (category) => {
+      if(category === 'all'){
+        setMenuItem(items);
+        return;
+      }
+      const newItems = items.filter((item) => 
+        item.category === category)
+        setMenuItem(newItems)
+  }
+
   return (
    <main>
-      <Menu />
-      <Categories />
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems = {fileterItems}/>
+        <Menu items = {menuItem} />
+      </section>
    </main>
   )
 }
--- 5-Menu/src/Categories.jsx
+++ 5-Menu/src/Categories.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems}) => {
+  return (
+    <main>
+      <div className='btn-container'>
+        <button className="filter-btn" onClick={() => filterItems('all')}>All</button>
+        <button className="filter-btn" onClick={() => filterItems('breakfast')}>Breakfast</button>
+        <button className="filter-btn" onClick={() => filterItems('lunch')}>Lunch</button>
+        <button className="filter-btn" onClick={() => filterItems('shakes')}>Shakes</button>
+
+      </div>
+    </main>
+  )
 };
 
 export default Categories;
