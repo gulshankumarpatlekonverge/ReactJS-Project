bdcf4710b3a67133eb1cb7549a409605562d29a2
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-16 15:10:31 +0530

    final project 4

--- 4-Accordion/src/App.jsx
+++ 4-Accordion/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Question from './Question'
+import data from './data'
 
 function App() {
+  const [question, setQuestion] = useState(data);
+
   return (
     <main>
-      <h2>Accordian</h2>
+      <div className="container">
+        <h3>Questin and Answer about Login</h3>
+        <section className="info">
+          {
+            question.map(question => {
+              return <Question key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
     </main>
   )
 }
