6b627e00abbba5285e072138874709c1b8cfea2a
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-16 12:17:16 +0530

    finished project 2

--- 2-Tours/src/App.jsx
+++ 2-Tours/src/App.jsx
@@ -1,14 +1,65 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Tours from './Tours'
+import Loading from './Loading'
+import Tour from './Tour'
+import { useEffect } from 'react'
+
+const url = `https://course-api.com/react-tours-project`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTour = tours.filter(tour => tour.id !== id)
+    setTours(newTour)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+  // const response = await fetch(url);
+  // const tours = await response  .json();
+  // console.log(tours);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response  .json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(false);
+    }
+  }
+  
+
+  useEffect(() => {
+    fetchTours();
+  }, [])
+
+  if(loading){
+    return(
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if(tours.length === 0){
+    return <main>
+      <div className='title'>
+        <h2>No Tours Left</h2>
+        <button onClick = {fetchTours}>Refresh</button>
+      </div>
+    </main>
+  }
   return (
-   <div>
-    <h2>Tours Project Setup</h2>
-   </div>
+    <main>
+      <Tours  tours = { tours } removeTour = {removeTour}/>
+    </main>
   )
 }
 
--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # ReactJS-Project
 ReactJS Project
 
-    This repo consist of 15 React Projects from basics to the pro level.
+    This repo consist of 15 React Projects from basics to the pro level. This application is created using Vite+React.
+        Commands:
+            To create new app
+                > npm create vite@latest <project-name>
+            To install dependencies
+                > npm install
+            To run app
+                > npm run dev
 
     List of Projects:
         1. Birthday Remender
-        2.
+        2. Tours
+            API Links : https://course-api.netlify.app/api/react-tour-project
         3.
         4.
         5.
