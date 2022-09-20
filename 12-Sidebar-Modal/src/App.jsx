01edcf40b6ba0356b8ffb8179c9f29d5a8b84600
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-20 14:28:01 +0530

    initial setup project 12

---
+++ 12-Sidebar-Modal/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  
+
+  return (
+   <>
+   <h2>Sidebar Modal</h2>
+   </>
+  )
+}
+
+export default App
