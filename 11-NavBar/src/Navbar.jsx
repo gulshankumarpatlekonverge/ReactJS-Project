2d642deff69b2db58f35d2dcb4c58834e2aaf3f7
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-20 13:55:17 +0530

    basic approach on butto click

--- 11-NavBar/src/Navbar.jsx
+++ 11-NavBar/src/Navbar.jsx
@@ -4,40 +4,44 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
   return <nav>
           <div className="nav-center">
             <div className="nav-header">
               <img src={logo} alt="logo" />
-              <button className="nav-toggle">
+              <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
                 <FaBars />
               </button>
             </div>
-            <div className="lins-container">
-              <ul className="social-icons">
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-                <li>
-                  <a href="#">Products</a>
-                </li>
-              </ul>
-            </div>
+            {
+              showLinks && 
+                <div className="links-container show-container">
+                <ul className="links">
+                  {
+                    links.map((link) => {
+                    const {id, url, text} = link
+                        return (
+                          <li key={id}>
+                            <a href={url}>{text}</a>
+                          </li>
+                        )
+                    })
+                  }
+                </ul>
+              </div>
+            }
+           
             <ul className="social-icons">
-              <li>
-                <a href="https://www.twitter.com"><FaTwitter /></a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com"><FaTwitter /></a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com"><FaTwitter /></a>
-              </li>
+              {
+                social.map((social) => {
+                const {id, url, icon} = social
+                  return(
+                    <li key={id}>
+                      <a href={url}>{icon}</a>
+                    </li>
+                  )
+                })
+              }
             </ul>
             </div>
         </nav>
