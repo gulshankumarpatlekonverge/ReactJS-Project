a2c6c81a22d2503d2ec448247fe33e481d47671d
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-22 09:20:33 +0530

    increase decrease and reducer functionality

--- 14-cart/src/App.jsx
+++ 14-cart/src/App.jsx
@@ -7,7 +7,15 @@ import CartContainer from './CartContainer'
 import { useGlobalContext } from './context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {loading} = useGlobalContext();
+  if(loading){
+    return (
+      <div className='loading'>  
+      <h1>Loading...</h1>
+      </div>
+
+    )
+  }
 
   return (
    <main>
