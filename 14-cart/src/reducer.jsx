e81ecf329eca6796de4adef6f87100c80b7996e0
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-22 09:59:53 +0530

    loading and fetch functionality

--- 14-cart/src/reducer.jsx
+++ 14-cart/src/reducer.jsx
@@ -63,6 +63,21 @@ const reducer = (state, action) => {
         }
     }
 
+    if(action.type === 'LOADING'){
+        return {
+            ...state,
+            loading: true
+        }
+    }
+
+    if(action.type === 'DISPLAY_ITEMS'){
+        return {
+            ...state, 
+            cart: action.payload,
+            loading: false
+        }
+    }
+
     return state;
 }
 
